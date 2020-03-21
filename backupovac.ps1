223763d46a2a5bf737fa78fc37238a35fb8ff277
Pavel Bykov <krystof@bykov.eu>
2020-03-21 12:36:05 +0100

    test

--- backupovac.ps1
+++ backupovac.ps1
@@ -2,9 +2,13 @@ $NAPOSLEDY_ZALOHOVANO = (Get-Content -Path .\last_backup.log)
 $DNESNI_DATUM = (Get-Date -Format FileDate)
 
 if ($NAPOSLEDY_ZALOHOVANO -lt $DNESNI_DATUM) {
+ Get-Date -Format FileDate | Out-File -FilePath .\last_backup.log
  Copy-Item -Path $env:APPDATA\StardewValley\* -Recurse -Force
- git add .
+ git add -A .
  git commit -m "new backup"
  git push
- Get-Date -Format FileDate | Out-File -FilePath .\last_backup.log
+ Write-Host "Backed up."
+}
+else {
+ Write-Host "Already backed up today."
 }
