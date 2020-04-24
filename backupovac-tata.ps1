a6fc779e78613e2265f904512bb1819896172025
Pavel Bykov <krystof@bykov.eu>
2020-04-24 20:45:44 +0200

    tata backup

--- backupovac-tata.ps1
+++ backupovac-tata.ps1
@@ -1,15 +1,10 @@
 $NAPOSLEDY_ZALOHOVANO = (Get-Content -Path .\last_backup.log)
 $DNESNI_DATUM = (Get-Date -Format FileDate)
 
-if ($NAPOSLEDY_ZALOHOVANO -lt $DNESNI_DATUM) {
- Get-Date -Format FileDate | Out-File -FilePath .\last_backup.log
- Copy-Item -Path $env:APPDATA\HelloGames\NMS\* -Recurse -Force
- git add .
- git commit -m "tata backup"
- git push
- Write-Host "Backed up."
-}
-else {
- Write-Host "Already backed up today."
-}
+Get-Date -Format FileDate | Out-File -FilePath .\last_backup.log
+Copy-Item -Path $env:APPDATA\HelloGames\NMS\* -Recurse -Force
+git add .
+git commit -m "tata backup"
+git push
+Write-Host "Backed up."
 & '.\No Mans Sky.url'
